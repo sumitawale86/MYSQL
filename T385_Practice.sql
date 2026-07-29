c0b96870730a0b4f2ceb9e432628fac16b535db6
Rvdhonde <ravi.dhonde007@gmail.com>
2026-07-29 09:07:34 +0530

    MYSQL daily worksheet

---
+++ T385_Practice.sql
@@ -0,0 +1,60 @@
+show databases;
+create database BankingDB;
+use BankingDB;
+show tableS;
+
+Create table Customers
+(
+CustomerID INT,
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+Email VARCHAR(100),
+Phone VARCHAR(15),
+AccountCreationDate Date
+);
+show tables;
+drop table Customers;
+show tables;
+drop table customers;
+
+select * from customers;
+create table Accounts (
+AccountID INT,
+AccountType varchar(20),
+Balance DECIMAL(10,2)
+);
+
+DEsc ACCOUNTS;
+CREATE table Transactions (
+ TransactionID INT,
+ TransactionDate date,
+ Amount decimal(10,2),
+ TransactionType varchar (20)
+ );
+ show tables;
+ CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+ 
+ CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+
+show tables;
+c
+
+
+
+
+
